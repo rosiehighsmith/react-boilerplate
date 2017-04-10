19b8a6f6d7dfcbf20c4f67db7d91826a6ee3879b
rosiehighsmith <rosie.highsmith@gmail.com>
2017-04-10 07:59:32 -0700

    create components class based

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 if(process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
 
 ReactDOM.render(
-  <div>Hello world</div>,
-  document.getElementById('app')
+	<App />,
+  	document.getElementById('app'),
 );
+
+
